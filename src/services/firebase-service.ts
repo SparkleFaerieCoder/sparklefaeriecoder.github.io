81cff68f80dac9747abf4a7fb683a25e8e36d1d3
Alexandra Silcox <alex.silcox86@gmail.com>
2020-01-04 20:28:06 -0800

    wip env files setup

--- src/services/firebase-service.ts
+++ src/services/firebase-service.ts
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
+const {env} = process;
+
 const config = {
-  apiKey: 'AIzaSyCgqj_1mWK0ZqKhqYKF_PbYiFGc2fI38aY',
-  authDomain: 'spf-blog-service.firebaseapp.com',
-  databaseURL: 'https://spf-blog-service.firebaseio.com',
-  projectId: 'spf-blog-service',
-  storageBucket: 'spf-blog-service.appspot.com',
-  messagingSenderId: '503763428128',
-  appId: '1:503763428128:web:4c9b491922499760162d45',
-  measurementId: 'G-27E8CGG6XE',
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FB_AUTH,
+  databaseURL: env.FB_DB,
+  projectId: env.PID,
+  storageBucket: env.FB_STORAGE,
+  messagingSenderId: env.MSI,
+  appId: env.APID,
+  measurementId: env.MI,
 };
 
 firebase.initializeApp(config);
